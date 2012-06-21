4b9299df3c0dd3f23c520359cc728f7fb8642441
nicomp <nicomp@laptop>
2012-06-21 13:28:31 -0400

    hi

---
+++ GitWorking/src/HelloWorldPackage/HelloWorld.java
@@ -0,0 +1,14 @@
+package HelloWorldPackage;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		// comment
+
+	}
+
+}
